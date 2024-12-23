4ef80e70d632bb57f3f2858c27471153d216cba2
杨婷 <2183770554@qq.com>
2024-12-23 08:47:53 +0800

    我将所有后端代码删除，保留了前端代码，等我分工后你们就可以开始各自创建自己的模块

--- src/test/java/com/milotnt/GymManagementSystemApplicationTests.java
+++ src/test/java/com/milotnt/GymManagementSystemApplicationTests.java
@@ -1,7 +1,5 @@
 package com.milotnt;
 
-import com.milotnt.mapper.MemberMapper;
-import com.milotnt.pojo.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
