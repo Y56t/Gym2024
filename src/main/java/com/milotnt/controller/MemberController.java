3e9d6914ed3c47794ce717e2ed2a83c44aca5a7b
nujabes012 <18980380780@163.com>
2024-12-30 15:33:36 +0800

    会员卡查询

--- src/main/java/com/milotnt/controller/MemberController.java
+++ src/main/java/com/milotnt/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.milotnt.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.milotnt.entity.Member;
 import com.milotnt.service.IMemberService;
 import lombok.RequiredArgsConstructor;
@@ -35,7 +36,7 @@ public class MemberController {
     }
 
     /**
-     * 1.主页跳转会员卡查询主页面
+     * 2.主页跳转会员卡查询主页面
      * @return
      */
     @RequestMapping("/toSelByCard")
@@ -43,6 +44,10 @@ public class MemberController {
         return "selectByMemberAccount.html";
     }
 
+    /**
+     * 3.查询功能
+     * @return
+     */
     @PostMapping("/selByCard")
     public String selectByMemberAccount(@RequestParam String memberAccount, Model model) {
         // 构造查询条件
@@ -65,5 +70,58 @@ public class MemberController {
         // 返回视图名称
         return "selectByMemberAccount";
     }
+    /**
+     * 4.转到编辑页面
+     * @return
+     */
+    @RequestMapping("/toUpdateMember")
+    public String toUpdateMember(@RequestParam String memberAccount, Model model){
+        // 构造查询条件
+        QueryWrapper<Member> queryWrapper = new QueryWrapper<>();
 
+        if (memberAccount != null && !memberAccount.trim().isEmpty()) {
+            // 精确匹配会员卡号
+            queryWrapper.eq("member_account", memberAccount);
+        }
+
+        // 按办卡时间倒序排序
+        queryWrapper.orderByDesc("card_time");
+
+        // 查询会员信息
+        List<Member> memberList = memberService.list(queryWrapper);
+
+        // 将结果添加到 Model
+        model.addAttribute("memberList", memberList);
+        return "UpdateMember";
+    }
+
+    /**
+     * 5.编辑功能
+     * @return
+     */
+    @RequestMapping("/updateMember")
+    public String updateMember(@ModelAttribute Member member,String memberAccount,Model model){
+        UpdateWrapper<Member> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("member_account", member.getMemberAccount());
+        memberService.update(member, updateWrapper);
+        model.addAttribute("message", "修改成功！");
+
+        QueryWrapper<Member> queryWrapper = new QueryWrapper<>();
+
+        if (memberAccount != null && !memberAccount.trim().isEmpty()) {
+            // 精确匹配会员卡号
+            queryWrapper.eq("member_account", memberAccount);
+        }
+
+        // 按办卡时间倒序排序
+        queryWrapper.orderByDesc("card_time");
+
+        // 查询会员信息
+        List<Member> memberList = memberService.list(queryWrapper);
+
+        // 将结果添加到 Model
+        model.addAttribute("memberList", memberList);
+
+        return "UpdateMember";
+    }
 }
